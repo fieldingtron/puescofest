9460e433adfbe39950160f50e62c6a715c27667e
fieldingtron <fields@nuspace.com>
2018-10-18 12:22:12 -0300

    pfest change email changes

--- src/js/contact_me.js
+++ src/js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://jumprock.co/mail/gonkertron",  //pfest for puescofest
+        url: "https://jumprock.co/mail/pfest",  //pfest for puescofest
         type: "POST",
         data: {
           name: name,
